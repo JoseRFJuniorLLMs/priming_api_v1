a63f9e53ed33f47c032a684c1c810798c9d7e072
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-19 17:28:39 +0000

    update

--- src/Controller/StudentController.py
+++ src/Controller/StudentController.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from src.Model.Student import Student
 from src.Service.StudentService import StudentService
 from src.Service.LoginService import LoginService
 
 app = APIRouter()
 
+DEFAULT_PAGE = 0
+DEFAULT_PAGE_SIZE = 10
 
 @app.post("/", response_model=Student, status_code=201)
 async def create_student(student: Student, current_user: str = Depends(LoginService.get_current_user)):
     return await StudentService.create(student)
 
+@app.get("/page", response_model=List[Student], status_code=200)
+async def get_students_paginated(page: int = Query(DEFAULT_PAGE, ge=0),
+                                  page_size: int = Query(DEFAULT_PAGE_SIZE, le=100),
+                                  current_user: str = Depends(LoginService.get_current_user)):
+    students = await StudentService.get_students_paginated(page, page_size)
+    return students
 
 @app.get("/{username}", response_model=Student, status_code=200)
 async def get_student_by_username(username: str, current_user: str = Depends(LoginService.get_current_user)):
