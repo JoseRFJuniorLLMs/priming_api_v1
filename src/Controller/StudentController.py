6346ae6e24263f719ea6d61a31e34bbda2782002
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-19 22:19:11 +0000

    update StudentController.py

--- src/Controller/StudentController.py
+++ src/Controller/StudentController.py
@@ -21,14 +21,14 @@ def get_current_user(token: str = Depends(LoginService().oauth2_scheme)):
 @app.post("/", response_model=Student, status_code=201)
 async def create_student(student: Student, current_user: str = Depends(get_current_user)):
     return await StudentService.create(student)
-
+"""
 @app.get("/page", response_model=List[Student], status_code=200)
 async def get_students_paginated(page: int = Query(DEFAULT_PAGE, ge=0),
                                   page_size: int = Query(DEFAULT_PAGE_SIZE, le=100),
                                   current_user: str = Depends(LoginService.get_current_user)):
     students = await StudentService.get_students_paginated(page, page_size)
     return students
-
+"""
 @app.get("/{username}", response_model=Student, status_code=200)
 @LoginService.validate_login_or_google
 async def get_student_by_username(request: Request):
