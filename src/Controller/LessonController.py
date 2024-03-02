a95e838006d2772d38b9ea358ee031dbcdbf7165
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-02 03:24:53 -0400

    feat: created endpoints to get lesson by course or by id

--- src/Controller/LessonController.py
+++ src/Controller/LessonController.py
@@ -6,7 +6,19 @@ from src.Service.LessonService import LessonService
 app = APIRouter()
 
 
-@app.get("/{course_id}", status_code=200)
+@app.get("/course/{course_id}", status_code=200)
 async def lessons_by_course(course_id):
-    lessons = LessonService.get_lessons_by_course(course_id)
+    lessons = await LessonService.get_lessons_by_course(course_id)
+    for lesson in lessons:
+        lesson["lesson_id"] = str(lesson["lesson_id"])
+
+    return lessons
+
+
+@app.get("/{lesson_id}", status_code=200)
+async def lesson_by_id(lesson_id):
+    lessons = await LessonService.get_lesson_by_id(lesson_id)
+    for lesson in lessons:
+        lesson["lesson_id"] = str(lesson["lesson_id"])
+
     return lessons
