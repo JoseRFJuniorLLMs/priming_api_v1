111f040b64726216f99c0e9f71e2507f4a3a8389
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-02 11:11:48 -0400

    feat: added auth in endpoint of lessons

--- src/Controller/LessonController.py
+++ src/Controller/LessonController.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette.requests import Request
+from src.Service.LoginService import LoginService
 
 from src.Service.LessonService import LessonService
 
 app = APIRouter()
 
 
+def get_current_user(token: str = Depends(LoginService().oauth2_scheme)):
+    return LoginService().get_current_user(token)
+
+
 @app.get("/course/{course_id}", status_code=200)
-async def lessons_by_course(course_id):
+async def lessons_by_course(course_id, current_user: str = Depends(get_current_user)):
     lessons = await LessonService.get_lessons_by_course(course_id)
     for lesson in lessons:
         lesson["lesson_id"] = str(lesson["lesson_id"])
@@ -16,7 +21,7 @@ async def lessons_by_course(course_id):
 
 
 @app.get("/{lesson_id}", status_code=200)
-async def lesson_by_id(lesson_id):
+async def lesson_by_id(lesson_id, current_user: str = Depends(get_current_user)):
     lessons = await LessonService.get_lesson_by_id(lesson_id)
     for lesson in lessons:
         lesson["lesson_id"] = str(lesson["lesson_id"])
--- src/Controller/LessonDoneController.py
+++ src/Controller/LessonDoneController.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.Service.LessonDoneService import LessonDoneService
+from src.Service.LoginService import LoginService
 
 app = APIRouter()
 
 
+def get_current_user(token: str = Depends(LoginService().oauth2_scheme)):
+    return LoginService().get_current_user(token)
+
+
 @app.get("/student/{student_id}")
-async def get_lesson_done_by_student(student_id):
+async def get_lesson_done_by_student(student_id, current_user: str = Depends(get_current_user)):
     data = LessonDoneService.get_lesson_done_by_student(student_id)
     for item in data:
         item["_id"] = str(item["_id"])
