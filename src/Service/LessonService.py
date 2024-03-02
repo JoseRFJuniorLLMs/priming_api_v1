af7500f07dbc70f4e3c07566362aed030e545ec4
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-02 03:27:17 -0400

    feat: added service to get lesson by id

--- src/Service/LessonService.py
+++ src/Service/LessonService.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 from src.Model.Course import Course
 from src.Repository.LessonRepository import LessonRepository
 
@@ -8,3 +10,8 @@ class LessonService:
     async def get_lessons_by_course(course_id):
         lessons = LessonRepository().get_lessons_by_course(course_id)
         return lessons
+
+    @staticmethod
+    async def get_lesson_by_id(lesson_id):
+        lesson = LessonRepository().get_lesson_by_id(lesson_id)
+        return lesson
