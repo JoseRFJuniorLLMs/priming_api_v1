fae0558cda47d79e3a2d9d7260206c8d23cdafd1
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-03-05 08:39:29 +0000

    updae

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Depends
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
+from starlette.middleware.sessions import SessionMiddleware
+
 from src.Model.Student import Student
 from src.Model.Login import Login
 from src.Controller import LoginController, StudentController
 from src.Service.LoginService import LoginService
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="secret")
 
 
 @app.on_event("startup")
@@ -16,11 +19,13 @@ async def startup_event():
 
 
 # Include student routes
-app.include_router(StudentController.app, prefix="/student", tags=["students"],
-                   dependencies=[Depends(LoginService.get_current_user)])
+app.include_router(StudentController.app, prefix="/student", tags=["students"])
 
 # Include login routes
 app.include_router(LoginController.app, prefix="", tags=["login"])
 
+app.include_router(LoginController.app, prefix="/lessons", tags=["lessons"])
+
 if __name__ == "__main__":
     import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
