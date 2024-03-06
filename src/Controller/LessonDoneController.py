bc0347374bb7378a6054534f6bd1446e429372e2
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-06 12:39:08 -0400

    fix: return function in await mode to no return warnings

--- src/Controller/LessonDoneController.py
+++ src/Controller/LessonDoneController.py
@@ -11,7 +11,7 @@ def get_current_user(token: str = Depends(LoginService().oauth2_scheme)):
 
 @app.get("/student/{student_id}")
 async def get_lesson_done_by_student(student_id, current_user: str = Depends(get_current_user)):
-    data = LessonDoneService.get_lesson_done_by_student(student_id)
+    data = await LessonDoneService.get_lesson_done_by_student(student_id)
     for item in data:
         item["_id"] = str(item["_id"])
         for i in item['lesson_done_details']:
