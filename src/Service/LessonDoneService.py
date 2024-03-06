1e906e3288bd12ea47166fe09eb0da162490b396
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-06 12:38:33 -0400

    feat: transform function in asynchronous function

--- src/Service/LessonDoneService.py
+++ src/Service/LessonDoneService.py
@@ -3,6 +3,6 @@ from src.Repository.LessonDoneRepository import LessonDoneRepository
 
 class LessonDoneService:
     @staticmethod
-    def get_lesson_done_by_student(student_id):
+    async def get_lesson_done_by_student(student_id):
         data = LessonDoneRepository().get_lesson_done_by_student(student_id)
         return data
