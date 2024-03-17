b58eced5f9fdbca8b2fc24e0d8218c957d3b2a2e
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-17 18:44:57 -0400

    feat: added endpoints to get lessons by name and delete lessons

--- src/crud/lesson_routes.py
+++ src/crud/lesson_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, HTTPException
 from starlette import status
 
 from src.auth.jwt_bearer import JWTBearer
@@ -9,16 +9,17 @@ from src.model import Lesson
 api = APIRouter(prefix='/lesson')
 
 
-@api.get('/{name}')
-async def get_lesson(name: str):
-    student = service.get_lesson_by_name(name)
+@api.get('/{id}', dependencies=[Depends(JWTBearer())])
+async def get_lesson(id: str):
+    student = service.get_lesson_by_id(id)
     return student
 
 
-@api.get('/')
-async def get_lessons():
-    courses = service.get_lesson_list()
-    return courses
+@api.get('/', dependencies=[Depends(JWTBearer())])
+async def get_lessons(name: str | None = None):
+    if name:
+        return service.get_lesson_by_name(name)
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Parametros inválidos!')
 
 
 @api.post('/', status_code=status.HTTP_201_CREATED, dependencies=[Depends(JWTBearer())])
@@ -31,3 +32,6 @@ async def update_lesson(lesson: Lesson = Body(...)):
     service.update_lesson(lesson)
 
 
+@api.delete('/{lesson_id}', status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(JWTBearer())])
+async def delete_lesson(lesson_id: str):
+    service.delete_by_id(lesson_id)
