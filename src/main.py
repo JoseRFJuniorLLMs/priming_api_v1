9c3dfb23a68faab8ef172668d14a6f17bd888564
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-03-17 18:48:15 -0400

    feat: added endpoints to main file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
-from src.crud import student_routes, course_routes, lesson_routes, user_routes, phrase_routes
+from src.crud import (
+    student_routes,
+    course_routes,
+    lesson_routes,
+    user_routes,
+    phrase_routes,
+    note_routes,
+    tag_routes,
+    list_word_routes)
 from src.logger import config_log
 
 app = FastAPI()
@@ -9,5 +17,8 @@ app.include_router(course_routes.api)
 app.include_router(lesson_routes.api)
 app.include_router(user_routes.api)
 app.include_router(phrase_routes.api)
+app.include_router(note_routes.api)
+app.include_router(tag_routes.api)
+app.include_router(list_word_routes.api)
 
 config_log()
