9a2ea40efa5c4f32970c7dc36b2808b2d8507aa3
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-04-04 15:02:13 -0400

    update with envs

--- src/crud/files_routes.py
+++ src/crud/files_routes.py
@@ -1,12 +1,30 @@
 import io
+import os
+
 from pydub import AudioSegment
 from google.oauth2 import service_account
 from google.cloud import speech
 from fastapi import APIRouter, HTTPException, UploadFile, File
+from dotenv import load_dotenv
+
+load_dotenv()
 
 api = APIRouter(prefix='/files')
 
-credentials = service_account.Credentials.from_service_account_file('src/config/credentials.json')
+CREDENTIAL = {
+  "type": os.getenv('TYPE'),
+  "project_id": os.getenv('PROJECT_ID'),
+  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+  "private_key": os.getenv('PRIVATE_KEY'),
+  "client_email": os.getenv('CLIENT_EMAIL'),
+  "client_id": os.getenv('CLIENT_ID'),
+  "auth_uri": os.getenv('AUTH_URI'),
+  "token_uri": os.getenv('TOKEN_URI'),
+  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+  "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
+  "universe_domain": os.getenv('UNIVERSE_DOMAIN')
+}
+credentials = service_account.Credentials.from_service_account_info(CREDENTIAL)
 client = speech.SpeechClient(credentials=credentials)
 
 
