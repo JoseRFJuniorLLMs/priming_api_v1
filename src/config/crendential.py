ca19edb059f49f14d76f0260c290f002f639a10e
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-04-04 15:30:49 -0400

    update with envs

--- src/config/crendential.py
+++ src/config/crendential.py
@@ -1,20 +1,26 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from fastapi import HTTPException
 
+# Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
-
+# Definir variáveis de ambiente com valores padrão ou vazios como fallback
 CREDENTIAL = {
-  "type": os.getenv('TYPE'),
-  "project_id": str(os.getenv('PROJECT_ID')),
-  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-  "private_key": str(os.getenv('PRIVATE_KEY')),
-  "client_email": os.getenv('CLIENT_EMAIL'),
-  "client_id": os.getenv('CLIENT_ID'),
-  "auth_uri": os.getenv('AUTH_URI'),
-  "token_uri": os.getenv('TOKEN_URI'),
-  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
-  "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
-  "universe_domain": os.getenv('UNIVERSE_DOMAIN')
+    "type": os.getenv('TYPE', ''),
+    "project_id": os.getenv('PROJECT_ID', ''),
+    "private_key_id": os.getenv('PRIVATE_KEY_ID', ''),
+    "private_key": os.getenv('PRIVATE_KEY', ''),
+    "client_email": os.getenv('CLIENT_EMAIL', ''),
+    "client_id": os.getenv('CLIENT_ID', ''),
+    "auth_uri": os.getenv('AUTH_URI', ''),
+    "token_uri": os.getenv('TOKEN_URI', ''),
+    "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL', ''),
+    "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL', ''),
+    "universe_domain": os.getenv('UNIVERSE_DOMAIN', '')
 }
 
+# Tratar exceção ao carregar variáveis de ambiente
+for key, value in CREDENTIAL.items():
+    if not value:
+        raise HTTPException(status_code=500, detail=f'Variável de ambiente {key} não definida')
